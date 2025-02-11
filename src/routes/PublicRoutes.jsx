53a418db96f59ca4e641b93f22d7f1d4658074b0
Sourav Singh <matainja025@gmail.com>
2025-02-11 13:54:52 +0530

    second commit

--- src/routes/PublicRoutes.jsx
+++ src/routes/PublicRoutes.jsx
@@ -1,7 +1,5 @@
-import { Navigate } from "react-router-dom";
-import { getauthToken } from "../_helper/authentication";
+import Rea from "react";
 import Login from "../pages/login/index";
-import Registration from "../pages/registration/Index";
 import MultistepForm from "../pages/registration/Index";
 
 export const publicRoutes = [
@@ -13,5 +11,4 @@ export const publicRoutes = [
     path: "/signup",
     element: <MultistepForm />,
   },
-  // Add other public routes like FAQ here
 ];
